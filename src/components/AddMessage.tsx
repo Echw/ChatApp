10840ff4a3e0c15a4905de879954796393338fc4
EWA CHWEDCZUK <echwedczuk0@gmail.com>
2022-08-30 12:51:05 +0200

    add new message section

---
+++ src/components/AddMessage.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { BsFillEmojiSmileFill } from 'react-icons/bs';
+import { IoMdSend } from 'react-icons/io';
+import styled from 'styled-components';
+
+const AddMessage = () => {
+  return (
+    <NewMessageSection>
+      <button>
+        <BsFillEmojiSmileFill />
+      </button>
+      <input type="text" placeholder="Type message..." />
+      <button>
+        <IoMdSend />
+      </button>
+    </NewMessageSection>
+  );
+};
+
+export default AddMessage;
+
+const NewMessageSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: white;
+  padding: 1rem;
+  z-index: 1000;
+  width: 100%;
+
+  button {
+    font-size: 3rem;
+    background-color: transparent;
+    border: none;
+    color: #c2c2c2;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+  }
+  input {
+    padding: 0 2rem;
+    width: 100%;
+    height: 4rem;
+    font-size: 1.5rem;
+    background-color: transparent;
+    border: none;
+    color: #828282;
+    ::placeholder {
+      color: #dde0e0;
+    }
+  }
+`;
